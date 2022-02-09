7ea118fe5fd165a3d8d9d3c5d9680bc1c41a011e
Baangkok Vanijyananda <iambaangkok@gmail.com>
2022-02-09 13:27:35 +0700

    initial configuration of project settings

---
+++ src/main/java/com/maikw/CPE200ProjectCAREN/Cpe200ProjectCarenApplication.java
@@ -0,0 +1,13 @@
+package com.maikw.CPE200ProjectCAREN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cpe200ProjectCarenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cpe200ProjectCarenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maikw/CPE200ProjectCAREN/Cpe200ProjectCarenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maikw.CPE200ProjectCAREN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cpe200ProjectCarenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

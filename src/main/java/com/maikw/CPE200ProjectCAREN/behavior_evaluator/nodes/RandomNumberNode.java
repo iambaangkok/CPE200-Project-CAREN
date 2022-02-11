3b23c7235ef6782ece39820877c88b617b11c811
Baangkok Vanijyananda <iambaangkok@gmail.com>
2022-02-11 11:06:23 +0700

    change RandomNumberNode to extends VariableNode
    reason is so that implementation goes more in-line with the project specification's description of "random" grammar

--- src/main/java/com/maikw/CPE200ProjectCAREN/behavior_evaluator/nodes/RandomNumberNode.java
+++ src/main/java/com/maikw/CPE200ProjectCAREN/behavior_evaluator/nodes/RandomNumberNode.java
@@ -2,15 +2,21 @@ package com.maikw.CPE200ProjectCAREN.behavior_evaluator.nodes;
 
 import java.util.Random;
 
-public class RandomNumberNode implements Node{
+public class RandomNumberNode extends VariableNode{
     protected Random random = null;
 
     public RandomNumberNode(){
         random = new Random();
+        identifier = "random";
     }
 
     @Override
     public double evaluate() {
         return random.nextDouble()*100;
+    }
+
+    @Override
+    public void assignValue(double value){
+        // no-op
     }
 }

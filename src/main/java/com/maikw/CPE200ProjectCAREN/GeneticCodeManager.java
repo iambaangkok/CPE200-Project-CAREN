ae7ab5e6e1409f7320ba823142ecab45341687e0
Baangkok Vanijyananda <iambaangkok@gmail.com>
2022-02-11 18:25:56 +0700

    added a method to read genetic code from file into GeneticCodeManager

--- src/main/java/com/maikw/CPE200ProjectCAREN/GeneticCodeManager.java
+++ src/main/java/com/maikw/CPE200ProjectCAREN/GeneticCodeManager.java
@@ -1,9 +1,38 @@
 package com.maikw.CPE200ProjectCAREN;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class GeneticCodeManager {
 
 
-    private GeneticCodeManager(){
 
+    public GeneticCodeManager(){
+
+    }
+
+    /**
+     * Combine the genetic code file in to one single string.
+     * @param filePath the path to the specified genetic code
+     * @return 
+     */
+    public static String getAsString(String filePath){
+        StringBuilder sb = new StringBuilder("");
+
+        try (FileReader fr = new FileReader(filePath); Scanner s = new Scanner(fr);) {
+            while(s.hasNextLine()){
+                sb.append(s.nextLine());
+                sb.append("\n");
+            }
+        } catch (FileNotFoundException | NullPointerException e) {
+            System.out.println("File not found");
+            //e.printStackTrace();
+        } catch (IOException e) {
+            //e.printStackTrace();
+        } 
+
+        return sb.toString();
     }
 }

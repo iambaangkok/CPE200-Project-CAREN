37f18e8a3b0eee0039b4b248243a21373cecb142
rbzgamer <kuro123520@hotmail.com>
2022-02-14 14:53:09 +0700

    added UnitFactory(uncomplete)

--- src/main/java/com/maikw/CPE200ProjectCAREN/UnitFactory.java
+++ src/main/java/com/maikw/CPE200ProjectCAREN/UnitFactory.java
@@ -1,5 +1,11 @@
 package com.maikw.CPE200ProjectCAREN;
 
 public class UnitFactory {
-    
+    public Antibody createAntibody(){
+        return new Antibody();
+    }
+
+    public Virus createVirus(){
+        return new Virus();
+    }
 }

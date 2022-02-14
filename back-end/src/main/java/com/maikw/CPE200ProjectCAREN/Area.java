b6f03978b835a3b9f07d857de064ec879e90e724
rbzgamer <kuro123520@hotmail.com>
2022-02-15 01:27:07 +0700

    added area(uncomplete)

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/Area.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/Area.java
@@ -1,9 +1,43 @@
 package com.maikw.CPE200ProjectCAREN;
 
+import java.util.LinkedList;
+
 public class Area {
 
+    protected List<Unit> units;
+    protected List<Virus> viruses;
+    protected List<Antibody> antibodies;
 
-    private Area(){
+    public Area(){
+        this.units = new LinkedList<Unit>(); 
+        this.viruses = new LinkedList<Virus>(); 
+        this.antibodies = new LinkedList<Antibody>();
+    }
 
+    private void addUnit(Unit dummy){
+        this.units.add(dummy);
+    }
+
+    private void addVirus(Virus dummy){
+        this.viruses.add(dummy);
+    }
+
+    private void addAntibody(Antibody dummy){
+        this.antibodies.add(dummy);
+    }
+
+    // 3 level 0 = green light -> all good , 1 = yellow light -> antibodies:viruses = 1:3 , 2 = red light -> area taken
+    public int alertLevel(){ 
+        if(this.antibodies.size() == 0){
+            return 2;
+        }else if(3*this.antibodies.size() <= this.viruses.size()){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
+    public boolean isTaken(){
+        return this.alertLevel() == 2;
     }
 }

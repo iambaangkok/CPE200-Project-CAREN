42792020466cd6071311ba295a87bdd90e0d7519
rbzgamer <kuro123520@hotmail.com>
2022-02-14 14:51:35 +0700

    Added unimplement Antibody & Virus

---
+++ src/main/java/com/maikw/CPE200ProjectCAREN/Antibody.java
@@ -0,0 +1,8 @@
+package com.maikw.CPE200ProjectCAREN;
+
+public class Antibody extends Unit{
+    public Virus virusToSpawn(){
+        return new Virus();
+    }
+    
+}
---
+++ src/main/java/com/maikw/CPE200ProjectCAREN/Virus.java
@@ -0,0 +1,8 @@
+package com.maikw.CPE200ProjectCAREN;
+
+public class Virus extends Unit{
+    public int creditReward(){
+        return 0;
+    }
+    
+}

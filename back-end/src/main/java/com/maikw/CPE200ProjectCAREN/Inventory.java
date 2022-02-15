d18d2cb9b74145a0096f68c8c543ac773f8288d2
Nonthawt20 <Nonthawt20@gmail.com>
2022-02-15 22:53:47 +0700

    update Inventory
    เพิ่ม method

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/Inventory.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/Inventory.java
@@ -2,7 +2,40 @@ package com.maikw.CPE200ProjectCAREN;
 
 public class Inventory {
 
-    private Inventory(){
+    protected Integer meleeCount = 0 ;
+    protected Integer rangedCount  = 0;
+    protected Integer aoeCount = 0 ;
 
+
+
+    public Inventory(){
+
+    }
+
+
+    public void setMeleeCount(Integer meleeCount) {
+        this.meleeCount += meleeCount;
+    }
+
+    public void setRangedCount(Integer rangedCount) {
+        this.rangedCount += rangedCount;
+    }
+
+    public void setAoeCount(Integer aoeCount) {
+        this.aoeCount += aoeCount;
+    }
+
+    public Integer getMeleeCount() {
+        return meleeCount;
+    }
+
+
+    public Integer getRangedCount() {
+        return rangedCount;
+    }
+
+
+    public Integer getAoeCount() {
+        return aoeCount;
     }
 }

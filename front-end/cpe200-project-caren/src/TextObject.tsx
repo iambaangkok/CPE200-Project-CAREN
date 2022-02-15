90e936bbd9564a2a7386602874f3c0ea3a3fe965
Baangkok Vanijyananda <iambaangkok@gmail.com>
2022-02-16 01:19:23 +0700

    added support for baseline, size, multiline string to TextObject

--- front-end/cpe200-project-caren/src/TextObject.tsx
+++ front-end/cpe200-project-caren/src/TextObject.tsx
@@ -1,3 +1,4 @@
+import { throws } from "assert";
 
 const DEBUG = true;
 
@@ -6,13 +7,14 @@ class TextObject{
     canvas;
     context;
     text;
+    size;
     font;
     position;
-    align;
     color;
+    align;
+    baseline;
 
-
-    constructor(text : string, font : string, x : number, y : number, color? : string|undefined, align? : string|undefined){
+    constructor(text : string[], size : number, font : string, x : number, y : number, color? : string|undefined, align? : string|undefined, baseline? : string|undefined){
         this.canvas = document.querySelector('canvas');
         this.context = this.canvas!.getContext('2d');
         this.position = {
@@ -21,27 +23,37 @@ class TextObject{
         };
         
         this.text = text;
+        this.size = size;
         this.font = font;
 
         if(color === undefined){
-            this.color = "black";
+            this.color = "white";
         }else{
             this.color = color;
         }
 
         if(align === undefined){
-            this.align = "left";
+            this.align = "start";
         }else{
             this.align = align;
+        }
+
+        if(baseline === undefined){
+            this.baseline = "top";
+        }else{
+            this.baseline = baseline;
         }
     }
 
     draw(){
         if(DEBUG) console.log("draw")
-        this.context!.font = this.font;
+        this.context!.font = this.size + "px " + this.font;
         this.context!.fillStyle = this.color;
         this.context!.textAlign = this.align as CanvasTextAlign;
-        this.context!.fillText(this.text, this.position.x, this.position.y);
+        this.context!.textBaseline = this.baseline as CanvasTextBaseline;
+        for(var i = 0 ; i < this.text.length; ++i){
+            this.context!.fillText(this.text[i], this.position.x, this.position.y + i*(this.size)*2);
+        }
     }
 
     update(){

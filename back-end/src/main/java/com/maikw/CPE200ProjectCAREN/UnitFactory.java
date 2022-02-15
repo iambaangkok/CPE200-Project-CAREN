fb459ea342b60fe1af2b7676a1fd5610be9ad195
rbzgamer <kuro123520@hotmail.com>
2022-02-15 21:20:36 +0700

    added static

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/UnitFactory.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/UnitFactory.java
@@ -1,11 +1,11 @@
 package com.maikw.CPE200ProjectCAREN;
 
 public class UnitFactory {
-    public Antibody createAntibody(String name, String type){
+    public static Antibody createAntibody(String name, String type){
         return new Antibody(name, type);
     }
 
-    public Virus createVirus(String name, String type){
+    public static Virus createVirus(String name, String type){
         return new Virus(name, type);
     }
 }

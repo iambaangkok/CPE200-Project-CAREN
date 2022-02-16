f5318997764784b9e45c623dd7e8734a1e33802f
rbzgamer <kuro123520@hotmail.com>
2022-02-16 13:35:58 +0700

    deleted empty space and added constructor to Areas class

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/Areas.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/Areas.java
@@ -4,25 +4,22 @@ import java.util.ArrayList;
 
 import java.util.List;
 
-public class Areas extends Area{
+public class Areas{
 
-    protected List<Area> allAreas  = new ArrayList<Area>();
-
+    protected List<Area> allAreas;
 
     public Areas(){
-
+        allAreas = new ArrayList<Area>();
     }
 
     public void add(Area a){
         allAreas.add(a);
     }
 
-
     public void showAllAreas(){ System.out.println(allAreas); }
 
     public Area selectArea(Integer n){
-
-     return  allAreas.get(n);
+        return  allAreas.get(n);
     }
 
 

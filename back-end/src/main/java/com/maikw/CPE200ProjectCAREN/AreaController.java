1346af8e409ae858cb31a7d0535635a493b31fab
Baangkok Vanijyananda <iambaangkok@gmail.com>
2022-02-16 17:21:32 +0700

    back-end: added @CrossOrigin to AreaController

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/AreaController.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/AreaController.java
@@ -1,5 +1,6 @@
 package com.maikw.CPE200ProjectCAREN;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,16 +12,19 @@ public class AreaController {
     Area area2 = new Area("Area2");
     Area area3 = new Area("Area3");
 
+    @CrossOrigin
     @GetMapping(path = "/getarea1") // http://localhost:8080/area/getarea1
     public Area getArea1(){
         return area;
     }
 
+    @CrossOrigin
     @GetMapping(path = "/getarea2") // http://localhost:8080/area/getarea2
     public Area getArea2(){
         return area2;
     }
 
+    @CrossOrigin
     @GetMapping(path = "/getarea3") // http://localhost:8080/area/getarea3
     public Area getArea3(){
         return area3;

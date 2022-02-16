d1719b882a6b35ebfc1b1f74faa4a0a223af492e
rbzgamer <kuro123520@hotmail.com>
2022-02-16 13:33:01 +0700

    deleted empty space and added constructor to Wave class

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/Wave.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/Wave.java
@@ -4,27 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Wave {
+    protected List<Virus> area1Virus;
+    protected List<Virus> area2Virus;
+    protected List<Virus> area3Virus;
+    protected int waveNumber;
 
-
-    protected List<Virus> area1Virus  = new ArrayList<Virus>();
-    protected List<Virus> area2Virus  = new ArrayList<Virus>();
-    protected List<Virus> area3Virus  = new ArrayList<Virus>();
-
-
-
-
-
-    public Wave(){
-
+    public Wave(int waveNumber) {
+        this.area1Virus = new ArrayList<Virus>();
+        this.area2Virus = new ArrayList<Virus>();
+        this.area3Virus = new ArrayList<Virus>();
+        this.waveNumber = waveNumber;
     }
 
-
     public List<Virus> getAreaVirus(Integer number) {
-
-
-
-        if(number == 1 ){return area1Virus;}
-        else if(number == 2 ){return area2Virus;}
-        else {return area3Virus;}
-
+        if (number == 1) {
+            return area1Virus;
+        } else if (number == 2) {
+            return area2Virus;
+        } else {
+            return area3Virus;
+        }
+    }
 }

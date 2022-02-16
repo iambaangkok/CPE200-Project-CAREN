48ac4bb162e06752e6c1bf8f2fd97073521764f9
Nonthawt20 <Nonthawt20@gmail.com>
2022-02-16 12:04:00 +0700

    add field Class ZoomManager
    ยังไม่ได้เพิ่ม method ที่จำเป็น

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/ZoomManager.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/ZoomManager.java
@@ -1,8 +1,14 @@
 package com.maikw.CPE200ProjectCAREN;
 
 public class ZoomManager {
+    protected Integer attentionPositionX ;
+    protected Integer attentionPositionY ;
+    protected Integer minZoomFactor ;
+    protected Integer maxZoomFactor ;
+    protected Integer currentZoomFactor ;
 
-    private ZoomManager(){
+
+    public ZoomManager(){
 
     }
 }

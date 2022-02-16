775c8ec5c8e8078861f269cdd0db115139d53eea
rbzgamer <kuro123520@hotmail.com>
2022-02-16 14:28:29 +0700

    added implemented area api

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/AreaController.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/AreaController.java
@@ -4,6 +4,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-public class AreaController {
+import java.util.List;
 
+@RestController
+@RequestMapping(path = "/area") // http://localhost:8080/area
+public class AreaController {
+    Area area = new Area("Area1");
+    Area area2 = new Area("Area2");
+    Area area3 = new Area("Area3");
+
+
+    @GetMapping(path = "/getarea1")
+    public Area getArea1(){
+        return area;
+    }
+
+    @GetMapping(path = "/getarea2")
+    public Area getArea2(){
+        return area2;
+    }
 }

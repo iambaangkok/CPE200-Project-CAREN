00dad16d4e0ad70c4660322ebe1685c6ce2a150c
Nonthawt20 <Nonthawt20@gmail.com>
2022-02-16 12:04:52 +0700

    add field Class WaveManager
    เพิ่มเติม field

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/WaveManager.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/WaveManager.java
@@ -1,10 +1,23 @@
 package com.maikw.CPE200ProjectCAREN;
 
-public class WaveManager {
+import java.util.ArrayList;
+import java.util.List;
+
+public class WaveManager extends Wave{
+        protected Integer maxWaveCont ;
+        protected Integer currentWaveCount  = 0 ;
+        protected  Integer timeBetweenWave  = 0 ;
+        protected List<Wave> waveInfo = new ArrayList<Wave>();
+
 
 
 
     private WaveManager(){
 
     }
+
+
+
+
+
 }

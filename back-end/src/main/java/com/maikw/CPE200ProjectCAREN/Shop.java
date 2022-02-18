14895bdc4f2af1e51df352bd3666f252996f3f79
rbzgamer <kuro123520@hotmail.com>
2022-02-18 20:17:30 +0700

    added empty shop method and constructor

---
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/Shop.java
@@ -0,0 +1,29 @@
+package com.maikw.CPE200ProjectCAREN;
+
+public class Shop {
+    protected int maxCredit;
+    protected int currentCredit;
+    protected int meleePrice;
+    protected int rangedPrice;
+    protected int aoePrice;
+
+    public Shop() {
+        this.maxCredit = 9999;
+        this.currentCredit = 0;
+        this.meleePrice = 100;
+        this.rangedPrice = 200;
+        this.aoePrice = 150;
+    }
+
+    public void buyMelee(){
+
+    }
+
+    public void buyRanged(){
+
+    }
+
+    public void buyAOE(){
+        
+    }
+}

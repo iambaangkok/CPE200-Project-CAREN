73a9923670196d2d2a4fc250179de9b37fe0102a
rbzgamer <kuro123520@hotmail.com>
2022-02-18 20:17:12 +0700

    added empty zoom method

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/ZoomManager.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/ZoomManager.java
@@ -11,4 +11,48 @@ public class ZoomManager {
     public ZoomManager(){
 
     }
+
+    public void ZoomIn(){
+
+    }
+
+    public void ZoomOut(){
+
+    }
+
+    public void resetZoom(){
+
+    }
+
+    public Integer getMinZoomFactor() {
+        return minZoomFactor;
+    }
+
+    public Integer getMaxZoomFactor() {
+        return maxZoomFactor;
+    }
+
+    public Integer getAttentionPositionX() {
+        return attentionPositionX;
+    }
+
+    public void setAttentionPositionX(Integer attentionPositionX) {
+        this.attentionPositionX = attentionPositionX;
+    }
+
+    public Integer getAttentionPositionY() {
+        return attentionPositionY;
+    }
+
+    public void setAttentionPositionY(Integer attentionPositionY) {
+        this.attentionPositionY = attentionPositionY;
+    }
+
+    public Integer getCurrentZoomFactor() {
+        return currentZoomFactor;
+    }
+
+    public void setCurrentZoomFactor(Integer currentZoomFactor) {
+        this.currentZoomFactor = currentZoomFactor;
+    }
 }

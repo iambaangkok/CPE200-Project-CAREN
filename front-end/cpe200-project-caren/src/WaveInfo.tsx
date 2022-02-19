e6725edf9597db3e9bbb93dbdcfaffb0f7b1c33f
Baangkok Vanijyananda <iambaangkok@gmail.com>
2022-02-19 14:29:09 +0700

    front-end: added WaveInfo class, finished most waveinfo stuff

---
+++ front-end/cpe200-project-caren/src/WaveInfo.tsx
@@ -0,0 +1,51 @@
+import ImageObject from "./ImageObject";
+import TextObject from "./TextObject";
+
+const DEBUG = true;
+
+class WaveInfo{
+    triangle: ImageObject;
+    meleeIcon: ImageObject;
+    rangedIcon: ImageObject;
+    aoeIcon: ImageObject;
+    meleeText: TextObject;
+    rangedText: TextObject;
+    aoeText: TextObject;
+
+    constructor(triangle: ImageObject,
+        meleeIcon: ImageObject, rangedIcon: ImageObject, aoeIcon: ImageObject,
+        meleeText: TextObject, rangedText: TextObject, aoeText: TextObject){
+        
+        this.triangle = triangle;
+        this.meleeIcon = meleeIcon;
+        this.rangedIcon = rangedIcon;
+        this.aoeIcon = aoeIcon;
+        this.meleeText = meleeText;
+        this.rangedText = rangedText;
+        this.aoeText = aoeText;
+    }
+
+    public draw(){
+        if(DEBUG) console.log("draw waveinfo")
+        this.triangle.draw();
+        this.meleeIcon.draw();
+        this.rangedIcon.draw();
+        this.aoeIcon.draw();
+        this.meleeText.draw();
+        this.rangedText.draw();
+        this.aoeText.draw();
+        
+    }
+
+    public setAllField(area : {total: number, melee: number, ranged: number, aoe: number}){
+        this.meleeIcon.setState((area.melee > 0)? 1 : 0);
+        this.rangedIcon.setState((area.ranged > 0)? 1 : 0);
+        this.aoeIcon.setState((area.aoe > 0)? 1 : 0);
+        this.meleeText.setText([area.melee.toString()]);
+        this.rangedText.setText([area.ranged.toString()]);
+        this.aoeText.setText([area.aoe.toString()]);
+    }
+}
+
+
+export default WaveInfo;

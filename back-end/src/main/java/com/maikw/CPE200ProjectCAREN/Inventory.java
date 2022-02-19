6e3681a5a81f577d99f1e047ec60464a7b460ed5
rbzgamer <kuro123520@hotmail.com>
2022-02-19 14:28:08 +0700

    changed setter to increase and decrease and added canPickup method to Inventory

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/Inventory.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/Inventory.java
@@ -2,40 +2,56 @@ package com.maikw.CPE200ProjectCAREN;
 
 public class Inventory {
 
-    protected Integer meleeCount = 0 ;
-    protected Integer rangedCount  = 0;
-    protected Integer aoeCount = 0 ;
-
-
+    protected Integer meleeCount;
+    protected Integer rangedCount;
+    protected Integer aoeCount;
 
     public Inventory(){
-
-        this.meleeCount += meleeCount;
-        this.rangedCount += rangedCount;
-        this.aoeCount += aoeCount;
+        this.meleeCount = 0;
+        this.rangedCount = 0;
+        this.aoeCount = 0;
     }
 
-
-    public void setMeleeCount(Integer meleeCount) {
-        this.meleeCount += meleeCount;
+    public boolean canPickup(String type){
+        return switch (type) {
+            case "melee" -> meleeCount > 0;
+            case "ranged" -> rangedCount > 0;
+            case "aoe" -> aoeCount > 0;
+            default -> false;
+        };
     }
 
-    public void setRangedCount(Integer rangedCount) {
-        this.rangedCount += rangedCount;
+    public void decreaseMeleeCount() {
+        this.meleeCount--;
     }
 
-    public void setAoeCount(Integer aoeCount) {
-        this.aoeCount += aoeCount;
+    public void increaseMeleeCount() {
+        this.meleeCount++;
     }
+
+    public void decreaseRangedCount() {
+        this.rangedCount--;
+    }
+
+    public void increaseRangedCount() {
+        this.rangedCount++;
+    }
+
+    public void decreaseAoeCount() {
+        this.aoeCount--;
+    }
+
+    public void increaseAoeCount() {
+        this.aoeCount++;
+    }
+
     public Integer getMeleeCount() {
         return meleeCount;
     }
 
-
     public Integer getRangedCount() {
         return rangedCount;
     }
-
 
     public Integer getAoeCount() {
         return aoeCount;

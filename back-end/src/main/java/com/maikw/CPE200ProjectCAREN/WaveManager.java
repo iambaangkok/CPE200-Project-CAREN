9b53169de53ce05d99c998adcf4dbd228d94ce9a
Nonthawt20 <Nonthawt20@gmail.com>
2022-02-23 15:16:45 +0700

    fix การสร้าง method เล็กน้อย

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/WaveManager.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/WaveManager.java
@@ -1,56 +1,52 @@
 package com.maikw.CPE200ProjectCAREN;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class WaveManager {
         protected Integer maxWaveCount  = 5 ;
         protected Integer currentWaveCount  = 1 ;
         protected  Integer timeBetweenWave  = 20 ;
-        protected List<Wave> waveInfo ;
-
-
+        protected List<Virus> viruses ;
+        protected Map<String,List<Virus>> allwave ;
 
 
     public WaveManager(){
-        this.waveInfo =  new ArrayList<Wave>();
-        setWaveInfo();
+        this.viruses = new ArrayList<>();
+        this.allwave = new HashMap<>();
 
     }
 
-    public Wave createWave(int wavenumber,int area1melee, int area1ranged, int area1aoe){
-
-        Wave wave = new Wave(wavenumber);
+    public void createUnitVirus(int area1melee, int area1ranged, int area1aoe) {
 
         for(int i = 0 ; i < area1melee;i++){
-            wave.addWave(UnitFactory.createVirus("melee"));
+            viruses.add(UnitFactory.createVirus( "melee"));
+        }
+        for(int i = 0 ; i < area1ranged ;i++){
+            viruses.add(UnitFactory.createVirus( "ranged"));
+        }
+        for (int i = 0 ; i < area1aoe;i++){
+            viruses.add(UnitFactory.createVirus( "aoe"));
         }
 
-        for(int i = 0 ; i < area1ranged ; i++){
-            wave.addWave(UnitFactory.createVirus("ranged"));
-        }
+        allwave.put("Wave_"+ currentWaveCount.toString() ,viruses);
+        currentWaveCount+=1;
+        this.viruses = new ArrayList<>();
+    }
 
-        for (int i = 0 ; i < area1aoe; i++){
-            wave.addWave(UnitFactory.createVirus("aoe"));
-        }
-        currentWaveCount += 1 ;
-        return  wave;
+    public void genVirus() {
+
+        createUnitVirus(3,1,0);
+        createUnitVirus(5,1,0);
+        createUnitVirus(10,3,1);
+        createUnitVirus(10,5,3);
+        createUnitVirus(50,50,50);
+
     }
 
 
-    public void setWaveInfo(){
-        waveInfo.add(createWave(1,5,2,0));
-        waveInfo.add(createWave(2,7,3,1));
-        waveInfo.add(createWave(3,15,5,3));
-        waveInfo.add(createWave(4,15,7,4));
-        waveInfo.add(createWave(5,25,7,2));
-    }
-
-
-    public Wave getWaveInfo(){
-
-        return waveInfo.get(currentWaveCount);
-    }
 
     public Integer getMaxWaveCount() {
         return maxWaveCount;

484af311372a10c0cfe5d26b2149da77b349691e
Nonthawt20 <Nonthawt20@gmail.com>
2022-02-26 14:25:21 +0700

    TimeManager

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/TimeManager.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/TimeManager.java
@@ -1,5 +1,6 @@
 package com.maikw.CPE200ProjectCAREN;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TimeManager {
@@ -7,24 +8,30 @@ public class TimeManager {
     protected Double deltaTime ;
     protected Double slowDownMultiplier ;
     protected Double fastForwardMuliplier ;
-    protected List<String> timeSate ;
+    protected boolean pause;
+    protected List<Integer> timeSate ;
 
 
     public TimeManager(){
-
+        this.fps = 60.0;
+        this.deltaTime = 1.0 ;
+        this.fastForwardMuliplier = 0.5;
+        this.slowDownMultiplier = 1.5;
+        this.pause = true;
+        this.timeSate = new ArrayList<>(); this.timeSate.add(1) ;this.timeSate.add(5);this.timeSate.add(10); this.timeSate.add(15); this.timeSate.add(30);
     }
 
     public Double getFps(){
         return fps;
     }
 
+    public Boolean getpush(){
+        return pause;
+    }
     public Double getDeltaTime() {
         return deltaTime;
     }
 
-    public void setTimeSate(List<String> timeSate) {
-        this.timeSate = timeSate;
-    }
 
     public void setSlowDownMultiplier(Double slowDownMultiplier) {
         this.slowDownMultiplier = slowDownMultiplier;
@@ -33,5 +40,7 @@ public class TimeManager {
     public void setFastForwardMuliplier(Double fastForwardMuliplier) {
         this.fastForwardMuliplier = fastForwardMuliplier;
     }
-
+    public void setPush(Boolean pause){
+        this.pause = pause;
+    }
 }

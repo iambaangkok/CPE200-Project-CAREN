f9da170c99e3cff0e7051a958ee5b03913973e70
Nonthawt20 <Nonthawt20@gmail.com>
2022-02-26 22:11:22 +0700

    TimeManager is comพรีด

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/TimeController.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/TimeController.java
@@ -1,4 +1,22 @@
 package com.maikw.CPE200ProjectCAREN;
 
-public class TimeController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/TimeManager") // http://localhost:8080/TimeManager
+
+public class TimeController{
+     TimeManager timeManager = new TimeManager();
+     @CrossOrigin
+     @PostMapping
+     public TimeManager getTimeManager() { return timeManager; }
+
+    @CrossOrigin
+    @PostMapping(path = "/playbar")
+    public String playBar(@RequestBody String type){
+         timeManager.setInputType(type);
+         return "Now is" +type;
+
+    }
 }

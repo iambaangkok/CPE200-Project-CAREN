cce08c93c80c86930157517cabfda39c9f871409
Baangkok Vanijyananda <iambaangkok@gmail.com>
2022-03-13 15:43:56 +0700

    front-end: added UnitDiagrams, GrammarDiagrams

--- front-end/cpe200-project-caren/src/HomePage.tsx
+++ front-end/cpe200-project-caren/src/HomePage.tsx
@@ -5,10 +5,10 @@ import "@fontsource/press-start-2p";
 import './App.css';
 import Config from './Config';
 import Header from './Header';
+import UnitDiagrams from './UnitDiagrams';
+import GrammarDiagrams from './GrammarDiagrams';
 
-import p_units from './images/units_banner.png';
-import p_unitRange_diagram from './images/unitRange_diagram.png';
-import p_unitDirection_diagram from './images/unitDirection_diagram.png';
+;
 
 /////////////////////////////////////////////
 
@@ -38,45 +38,8 @@ class HomePage extends React.Component {
                 }}>
                 <Header></Header>
                 <App></App>
-                <img src={p_units} style={{paddingBottom:"60px", paddingTop:"60px"}}></img>
-                <div style={{
-                    justifyContent: "start", alignItems:"start",
-                    color:Config.COLOR_BONEWHITE
-                }}> 
-                    <div style={{
-                        fontFamily:"Press Start 2P", fontSize:"30px",
-                        color: Config.COLOR_LIGHTBLUE
-                        }}>
-                        Range 
-                        <p style={{
-                            fontSize: "24px",
-                            color: Config.COLOR_BONEWHITE,
-                            paddingLeft: "70px"
-                            }}>
-                            <div>Units have 3 detect ranges</div>
-                            <div>Each range is represented by a number</div>
-                            <div>_________</div>
-                            <div>Close - 1, Attack - 2, Detect - 3</div>
-                        </p>
-                        <img src={p_unitRange_diagram} style={{paddingBottom:"100px", paddingTop:"30px"}}></img>
-
-                        Direction
-                        <p style={{
-                            fontSize: "24px",
-                            color: Config.COLOR_BONEWHITE,
-                            paddingLeft: "70px",
-                            }}>
-                            <div>Units detect in a 45 degree range in each direction</div>
-                            <div>Each direction is represented by a number</div>
-                            <div>_________</div>
-                            <div>Up - 1, UpRight - 2, Right - 3, DownRight - 4</div>
-                            <div>Down - 5, DownLeft - 6, Left - 7, UpLeft - 8</div>
-                        </p>
-                        <img src={p_unitDirection_diagram} style={{paddingBottom:"100px", paddingTop:"30px"}}></img>
-                        
-
-                    </div>
-                </div>
+                <UnitDiagrams></UnitDiagrams>
+                <GrammarDiagrams></GrammarDiagrams>
             </div>
 		)
 	}

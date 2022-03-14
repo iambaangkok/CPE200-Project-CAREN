c2fc5ac87d089bd4f0d635f6e8a1a288866e181e
Nonthawt20 <Nonthawt20@gmail.com>
2022-03-14 20:00:13 +0700

    ปรับค่าเวลาให้เหมาะสมกับความเร็ว

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/TimeManager.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/TimeManager.java
@@ -8,7 +8,7 @@ public class TimeManager {
     protected Double deltaTime ;
     public Double slowDownMultiplier ;
     protected Double fastForwardMuliplier ;
-    protected String pause;
+//    protected String pause;
     protected String inputType;
     protected List<Integer> timeSate ;
 
@@ -18,35 +18,35 @@ public class TimeManager {
     public TimeManager(){
         this.fps = 60.0;
         this.deltaTime = 1.0 ;
-        this.fastForwardMuliplier = 5.0 ;
-        this.slowDownMultiplier = 0.5 ;
+        this.fastForwardMuliplier = 0.025 ;
+        this.slowDownMultiplier = 2.5 ;
         this.timeSate = new ArrayList<>(); this.timeSate.add(1) ;this.timeSate.add(5);this.timeSate.add(10); this.timeSate.add(15); this.timeSate.add(30);
-        this.pause = "pause";
-        this.inputType = "";
+//        this.pause = "pause";
+        this.inputType = "fastforward"; //fastforward
     }
 
     public Double getFps(){
         return fps;
     }
 
-    public String  getpush(){
-        return pause;
-    }
+//    public String  getpush(){
+//        return pause;
+//    }
     public Double getDeltaTime() {
         return deltaTime;
     }
 
 
     public void setSlowDownMultiplier(Double slowDownMultiplier) {
-        this.slowDownMultiplier = slowDownMultiplier;
+        this.slowDownMultiplier = slowDownMultiplier*2;
     }
 
     public void setFastForwardMuliplier(Double fastForwardMuliplier) {
-        this.fastForwardMuliplier = fastForwardMuliplier;
+        this.fastForwardMuliplier = fastForwardMuliplier/2;
     }
-    public void setPush(String pause){
-        this.pause = pause;
-    }
+//    public void setPush(String pause){
+//        this.pause = pause;
+//    }
 
 
     public String getInput(){

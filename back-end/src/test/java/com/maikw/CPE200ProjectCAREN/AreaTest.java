3e53fba391c144b8bb73244d58f3928ea95e5903
rbzgamer <kuro123520@hotmail.com>
2022-03-14 14:33:15 +0700

    finish @Test addVirus and addAllVirus

--- back-end/src/test/java/com/maikw/CPE200ProjectCAREN/AreaTest.java
+++ back-end/src/test/java/com/maikw/CPE200ProjectCAREN/AreaTest.java
@@ -2,23 +2,44 @@ package com.maikw.CPE200ProjectCAREN;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AreaTest {
-
-    @Test
-    public void validTest(){
-
-    }
-
-    @Test
-    void addUnit() {
-    }
+    Area area = new Area("Area1");
+    Antibody[] abs_melee = {UnitFactory.createAntibody("melee"),UnitFactory.createAntibody("melee"),UnitFactory.createAntibody("melee")};
+    Antibody[] abs_ranged = {UnitFactory.createAntibody("ranged"),UnitFactory.createAntibody("ranged"),UnitFactory.createAntibody("ranged")};
+    Antibody[] abs_aoe = {UnitFactory.createAntibody("aoe"),UnitFactory.createAntibody("aoe"),UnitFactory.createAntibody("aoe")};
+    Virus[] vs_melee = {UnitFactory.createVirus("melee"),UnitFactory.createVirus("melee"),UnitFactory.createVirus("melee")};
+    Virus[] vs_ranged = {UnitFactory.createVirus("ranged"),UnitFactory.createVirus("ranged"),UnitFactory.createVirus("ranged")};
+    Virus[] vs_aoe = {UnitFactory.createVirus("aoe"),UnitFactory.createVirus("aoe"),UnitFactory.createVirus("aoe")};
+    List<Virus> viruses = new ArrayList<Virus>();
+    List<Antibody> antibodies = new ArrayList<Antibody>();
 
     @Test
     void addVirus() {
+        for(int i = 0 ; i < 3 ; i++){
+            area.addVirus(vs_melee[i]);
+            assertEquals(vs_melee[i].getArea(),area);
+            area.addVirus(vs_ranged[i]);
+            assertEquals(vs_ranged[i].getArea(),area);
+            area.addVirus(vs_aoe[i]);
+            assertEquals(vs_aoe[i].getArea(),area);
+        }
+        assertEquals(area.viruses.size(),9);
     }
 
     @Test
     void addAllVirus() {
+        for(int i = 0; i < 3 ; i++){
+            viruses.add(vs_melee[i]);
+            viruses.add(vs_ranged[i]);
+            viruses.add(vs_aoe[i]);
+        }
+        area.addAllVirus(viruses);
+        assertEquals(area.getViruses(),viruses);
     }
 
     @Test
@@ -27,10 +48,6 @@ public class AreaTest {
 
     @Test
     void addAllAntibody() {
-    }
-
-    @Test
-    void removeUnit() {
     }
 
     @Test
@@ -46,10 +63,6 @@ public class AreaTest {
     }
 
     @Test
-    void evaluate() {
-    }
-
-    @Test
     void canPlace() {
     }
 
@@ -58,6 +71,6 @@ public class AreaTest {
     }
 
     @Test
-    void snapViruse() {
+    void snapViruses() {
     }
 }

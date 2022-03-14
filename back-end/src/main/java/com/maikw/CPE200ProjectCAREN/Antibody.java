d08b19e6fa0f7c3937175d1cf659388ab72771a7
Nonthawt20 <Nonthawt20@gmail.com>
2022-03-14 23:02:07 +0700

    virusToSpawn เมื่อแอนติบอดี้ตาย ไวรัสจะไปเกิดใหม่ในต่างโลก

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/Antibody.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/Antibody.java
@@ -1,23 +1,21 @@
 package com.maikw.CPE200ProjectCAREN;
 
+import java.util.List;
+
 public class Antibody extends Unit{
     public Antibody(String name, String type, String geneticCode) {
         super(name, type, geneticCode);
         this.unitClass = "antibody";
     }
 
-    @Override
-    public void takeDamage(int dmg){
-        super.takeDamage(dmg);
-        if(this.getCurrentHealth() <= 0) virusToSpawn();
-    }
-
-    public void virusToSpawn(){
+    public void virusToSpawn(List<Virus> q){
         Virus reborn = UnitFactory.createVirus(this.getType());
         this.area.removeAntibody(this);
-        this.area.addVirus(reborn);
+//        this.area.addVirus(reborn);
+        q.add(reborn);
         reborn.setPositionX(this.getPositionX());
         reborn.setPositionY(this.getPositionY());
+
     }
     
 }

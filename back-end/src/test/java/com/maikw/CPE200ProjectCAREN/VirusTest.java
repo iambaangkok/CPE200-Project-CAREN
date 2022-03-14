945836b60c736d90f11e795a4dd45732f24c09e5
rbzgamer <kuro123520@hotmail.com>
2022-03-15 03:15:15 +0700

    finished VirusTest

--- back-end/src/test/java/com/maikw/CPE200ProjectCAREN/VirusTest.java
+++ back-end/src/test/java/com/maikw/CPE200ProjectCAREN/VirusTest.java
@@ -2,8 +2,17 @@ package com.maikw.CPE200ProjectCAREN;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class VirusTest {
+    Virus virus = UnitFactory.createVirus("melee");
+    Antibody antibodyGod = UnitFactory.createAntibody("melee");
+
     @Test
     void creditReward() {
+        while(virus.getCurrentHealth() > 0){
+            antibodyGod.attack(virus);
+        }
+        assertEquals(virus.creditReward(), 50);
     }
 }

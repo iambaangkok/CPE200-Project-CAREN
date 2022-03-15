04ea78677dc0d237a74809e4bc78c679a9b4ec42
Nonthawt20 <Nonthawt20@gmail.com>
2022-03-15 11:16:52 +0700

    เพิ่ม API

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/GameHandler.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/GameHandler.java
@@ -1,32 +1,35 @@
 package com.maikw.CPE200ProjectCAREN;
 
 
+import com.maikw.CPE200ProjectCAREN.apiclasses.ApiData_Base;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
 @Component
+@RestController
+@RequestMapping(path = "/gamehandler") // http://localhost:8080/gamehandler
 public class GameHandler {
-
 
     protected String id ;
     protected Map<String,Thread> map ;
     protected Map<String, Game> gameMap;
 
+    @Autowired
     public GameHandler(String id){
         this.id = id ;
         map = new HashMap<>();
         gameMap = new HashMap<>();
     }
 
-    public static void main(String[] args) {
-        Thread t1 = new Thread(new Game());
-
-    }
 
     //api
+    @CrossOrigin
+    @PostMapping(path = "/checkid") // http://localhost:8080/gamehandler/checkid
     public String checkId(String id){
         for(String ID : map.keySet()){
             if(id.equals(ID)){
@@ -41,7 +44,9 @@ public class GameHandler {
         return id;
     }
 
-    //api
+
+    @CrossOrigin
+    @PostMapping(path = "/runGame") // http://localhost:8080/gamehandler/runGame
     public void runGame(String id ){
         if(map.containsKey(id)){
             map.get(id).start();
@@ -53,11 +58,13 @@ public class GameHandler {
         }
     }
 
-    public Game getGame(String id){
-        if(!map.containsKey(id)){
-            runGame(id);
+    @CrossOrigin
+    @PostMapping(path = "/getGame") // http://localhost:8080/gamehandler/getGame
+    public Game getGame(@RequestBody ApiData_Base  data){
+        if(!map.containsKey(data.getId())){
+            runGame(data.getId());
         }
-        return gameMap.get(id);
+        return gameMap.get(data.getId());
     }
 
 }

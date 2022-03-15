a394f4dd8b2501f93f13af546d79d02728f586dd
Nonthawt20 <Nonthawt20@gmail.com>
2022-03-15 15:12:10 +0700

    APIData_Area

---
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/apiclasses/APIData_Area.java
@@ -0,0 +1,41 @@
+package com.maikw.CPE200ProjectCAREN.apiclasses;
+
+public class APIData_Area {
+    private int total;
+    private int melee;
+    private int ranged;
+    private int aoe;
+
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getMelee() {
+        return melee;
+    }
+
+    public void setMelee(int melee) {
+        this.melee = melee;
+    }
+
+    public int getRanged() {
+        return ranged;
+    }
+
+    public void setRanged(int ranged) {
+        this.ranged = ranged;
+    }
+
+    public int getAoe() {
+        return aoe;
+    }
+
+    public void setAoe(int aoe) {
+        this.aoe = aoe;
+    }
+}

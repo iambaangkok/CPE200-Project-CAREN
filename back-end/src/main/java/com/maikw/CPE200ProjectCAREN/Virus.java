cf5ba0240d37a545d2232f06215d686fb054d1d3
rbzgamer <kuro123520@hotmail.com>
2022-03-15 19:00:59 +0700

    fixed random position in Virus

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/Virus.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/Virus.java
@@ -8,9 +8,14 @@ public class Virus extends Unit{
     public Virus(String name, String type, String geneticCode) {
         super(name, type, geneticCode);
         super.unitClass = "virus";
+        Unit dummy = UnitFactory.createDummy("melee");
         Random r = new Random();
         double valX = (-100.0) + (100.0 - (-100.0)) * r.nextDouble();
         double valY = (-100.0) + (100.0 - (-100.0)) * r.nextDouble();
+        while(range(dummy, this) > 100){
+            valX = (-100.0) + (100.0 - (-100.0)) * r.nextDouble();
+            valY = (-100.0) + (100.0 - (-100.0)) * r.nextDouble();
+        }
         this.positionX = (double) (Math.round(valX*10.0)/10.0);
         this.positionY = (double) (Math.round(valY*10.0)/10.0);
     }

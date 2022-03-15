55b469f5ef6dbddf65a7765e25b8d652948767cd
Baangkok Vanijyananda <iambaangkok@gmail.com>
2022-03-15 16:24:02 +0700

    back-end: added APIs for get-setting speedMultiplier

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/TimeController.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/TimeController.java
@@ -1,5 +1,8 @@
 package com.maikw.CPE200ProjectCAREN;
 
+import com.maikw.CPE200ProjectCAREN.apiclasses.ApiData_Base;
+import com.maikw.CPE200ProjectCAREN.apiclasses.ApiData_BuyUnit;
+import com.maikw.CPE200ProjectCAREN.apiclasses.ApiData_SpeedMultiplierReturnData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,16 +10,52 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/timemanager") // http://localhost:8080/TimeManager
 
 public class TimeController{
-     TimeManager timeManager = new TimeManager();
-     @CrossOrigin
-     @PostMapping
-     public TimeManager getTimeManager() { return timeManager; }
+    private final GameHandler gameHandler;
+
+    @Autowired
+    public TimeController(GameHandler gameHandler){
+        this.gameHandler = gameHandler;
+    }
+
+//    @CrossOrigin
+//    @PostMapping(path = "/playbar")
+//    public String playBar(@RequestBody String type){
+//         timeManager.setInputType(type);
+//         return "Now is" +type;
+//    }
 
     @CrossOrigin
-    @PostMapping(path = "/playbar")
-    public String playBar(@RequestBody String type){
-         timeManager.setInputType(type);
-         return "Now is" +type;
+    @PostMapping(path = "/getspeedmultiplier")
+    public ApiData_SpeedMultiplierReturnData getSpeedMultiplier(@RequestBody ApiData_Base data){
+        Game game = gameHandler.getGame(data);
+        TimeManager timeManager = game.getTimeManager();
 
+        ApiData_SpeedMultiplierReturnData returnData = new ApiData_SpeedMultiplierReturnData();
+        returnData.setType(timeManager.getInputType());
+        String type = returnData.getType();
+        switch (type) {
+            case "pause":
+                returnData.setMultiplier(0);
+                break;
+            case "fastforward":
+                returnData.setMultiplier(timeManager.getFastForwardMuliplier());
+                break;
+            case "slowdown":
+                returnData.setMultiplier(timeManager.getSlowDownMultiplier());
+                break;
+            default:
+                returnData.setType("normal");
+                returnData.setMultiplier(0);
+                break;
+        }
+        return returnData;
+    }
+
+    @CrossOrigin
+    @PostMapping(path = "/setspeedmultiplier")
+    public void setSpeedMultiplier(@RequestBody ApiData_BuyUnit data){
+        Game game = gameHandler.getGame(data);
+        TimeManager timeManager = game.getTimeManager();
+        timeManager.setInputType(data.getType());
     }
 }
---
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/apiclasses/ApiData_SpeedMultiplierReturnData.java
@@ -0,0 +1,22 @@
+package com.maikw.CPE200ProjectCAREN.apiclasses;
+
+public class ApiData_SpeedMultiplierReturnData {
+    private String type;
+    private double multiplier;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getMultiplier() {
+        return multiplier;
+    }
+
+    public void setMultiplier(double multiplier) {
+        this.multiplier = multiplier;
+    }
+}

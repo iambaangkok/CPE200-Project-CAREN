5120f207956a3dce1b524edf09813bccec639440
Nonthawt20 <Nonthawt20@gmail.com>
2022-03-15 11:58:06 +0700

    API ของ WaveManager เพิ่มในส่วนของ currentWave

---
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/apiclasses/ApiData_Wave.java
@@ -0,0 +1,13 @@
+package com.maikw.CPE200ProjectCAREN.apiclasses;
+
+public class ApiData_Wave extends ApiData_Base{
+
+    public int currentWave  ;
+
+    public int getCurrentWave(){
+        return currentWave;
+    }
+
+
+    public void setCurrentWave(int currentWave){this.currentWave = currentWave;}
+}

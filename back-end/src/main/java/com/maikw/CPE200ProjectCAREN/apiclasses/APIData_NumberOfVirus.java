8a434760cf0a48cda775f4ffca51d64a7be62973
Nonthawt20 <Nonthawt20@gmail.com>
2022-03-15 15:12:33 +0700

    APIData_NumberOfVirus

---
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/apiclasses/APIData_NumberOfVirus.java
@@ -0,0 +1,42 @@
+package com.maikw.CPE200ProjectCAREN.apiclasses;
+
+public class APIData_NumberOfVirus   {
+
+    public int wavenumbre ;
+    public APIData_Area area1;
+    public APIData_Area area2;
+    public APIData_Area area3;
+
+
+    public int getWavenumbre() {
+        return wavenumbre;
+    }
+
+    public void setWavenumbre(int wavenumbre) {
+        this.wavenumbre = wavenumbre;
+    }
+
+    public APIData_Area getArea1() {
+        return area1;
+    }
+
+    public void setArea1(APIData_Area area1) {
+        this.area1 = area1;
+    }
+
+    public APIData_Area getArea2() {
+        return area2;
+    }
+
+    public void setArea2(APIData_Area area2) {
+        this.area2 = area2;
+    }
+
+    public APIData_Area getArea3() {
+        return area3;
+    }
+
+    public void setArea3(APIData_Area area3) {
+        this.area3 = area3;
+    }
+}

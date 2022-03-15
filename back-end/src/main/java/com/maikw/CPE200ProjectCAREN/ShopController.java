f005718bdcc3597786415423da81c69c15037cd6
rbzgamer <kuro123520@hotmail.com>
2022-03-15 11:45:08 +0700

    added gameHandler to ShopController for select ID game

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/ShopController.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/ShopController.java
@@ -1,22 +1,32 @@
 package com.maikw.CPE200ProjectCAREN;
 
+import com.maikw.CPE200ProjectCAREN.apiclasses.ApiData_Base;
+import com.maikw.CPE200ProjectCAREN.apiclasses.ApiData_BuyUnit;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/shop") // http://localhost:8080/shop
 public class ShopController {
-    Shop shop = new Shop();
+    GameHandler gameHandler;
+
+    @Autowired
+    public ShopController(GameHandler gameHandler){
+        this.gameHandler = gameHandler;
+    }
 
     @CrossOrigin
-    @GetMapping
-    public Shop getShop(){
+    @PostMapping(path = "/getshop")
+    public Shop getShop(@RequestBody ApiData_Base data){
+        Shop shop = gameHandler.getGame(data).getShop();
         return shop;
     }
 
     @CrossOrigin
     @PostMapping(path = "/buyunit")
-    public String buyUnit(@RequestBody String type){
-        switch (type) {
+    public String buyUnit(@RequestBody ApiData_BuyUnit data){
+        Shop shop = gameHandler.getGame(data).getShop();
+        switch (data.getType()) {
             case "melee" -> {
                 shop.buyMelee();
                 return "Buy Melee Success";

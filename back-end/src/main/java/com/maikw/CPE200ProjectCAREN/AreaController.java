0d8b14e74841bc5520fe341a3cd31a984063b691
rbzgamer <kuro123520@hotmail.com>
2022-03-15 11:15:25 +0700

    added gameHandler to AreaController for select ID game

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/AreaController.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/AreaController.java
@@ -1,40 +1,43 @@
 package com.maikw.CPE200ProjectCAREN;
 
+import com.maikw.CPE200ProjectCAREN.apiclasses.ApiData_Base;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/area") // http://localhost:8080/area
 public class AreaController {
-    private final Area area;
-    private final Area area2;
-    private final Area area3;
+    private Area area;
+    private Area area2;
+    private Area area3;
+    private final GameHandler gameHandler;
 
     @Autowired
-    public AreaController(Game game) {
-        this.area = game.areas.get(0);
-        this.area2 = game.areas.get(1);
-        this.area3 = game.areas.get(2);
+    public AreaController(GameHandler gameHandler) {
+        this.gameHandler = gameHandler;
     }
 
     @CrossOrigin
     @GetMapping(path = "/getarea1") // http://localhost:8080/area/getarea1
-    public Area getArea1(){
+    public Area getArea1(@RequestBody ApiData_Base data){
+        Game game = gameHandler.getGame(data.getId());
+        this.area = game.areas.get(0);
         return area;
     }
 
     @CrossOrigin
     @GetMapping(path = "/getarea2") // http://localhost:8080/area/getarea2
-    public Area getArea2(){
+    public Area getArea2(@RequestBody ApiData_Base data){
+        Game game = gameHandler.getGame(data.getId());
+        this.area2 = game.areas.get(1);
         return area2;
     }
 
     @CrossOrigin
     @GetMapping(path = "/getarea3") // http://localhost:8080/area/getarea3
-    public Area getArea3(){
+    public Area getArea3(@RequestBody ApiData_Base data){
+        Game game = gameHandler.getGame(data.getId());
+        this.area3 = game.areas.get(2);
         return area3;
     }
 }
